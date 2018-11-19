c119b77c48096584f73c2a3dd217e944469d5f21
Till <vm06381592966573992v@vmail.inf.h-brs.de>
2018-11-19 13:02:55 +0100

    Interrupt auf steigende oder fallende Flanke eines Buttons funktionsfähig

--- u01-led-taster/Src/main.cpp
+++ u01-led-taster/Src/main.cpp
@@ -11,40 +11,43 @@
 #define BTN1_PIN 10
 //*******************************************************************
 
+// Interrupt-Handler müssen als C-Funktionen deklariert werden
 extern "C" {
-	void EINT0_IRQHandler(void) {
-		// Clear Interrupt Flag
-		LPC_SC->EXTINT = (1 << 0);
-		PinGPIO led1 = PinGPIO(LED1_PORT, LED1_PIN, true);
-		led1.toggle();
-	}
+	void EINT0_IRQHandler(void);
 }
+
+// Interrupt Service Routine für EINT0
+void EINT0_IRQHandler(void) {
+	// Clear Interrupt Flag
+	LPC_SC->EXTINT = (1 << 0);
+	
+	// Toggle LED
+	PinGPIO led1 = PinGPIO(LED1_PORT, LED1_PIN, true);
+	led1.toggle();
+}
+
 int main(void) {
 	PinGPIO led1 = PinGPIO(LED1_PORT, LED1_PIN, true);
-		
-	// Pin 2.10 als Interrupt-Quelle einrichten
-	LPC_PINCON->PINSEL4 |= (0x01 << 20);
-
-	// TODO: Interrupt triggert bei fallender Flanke und danach,
-	// bis zur nächsten steigenden Flanke!
 	
-	// Interrupt 2.10 auf fallende Flanke setzen
-	LPC_GPIOINT->IO2IntEnF |= (1 << 10);
-	// Interrupt 2.10 auf steigende Flanke setzen
-	//LPC_GPIOINT->IO2IntEnR |= (1 << 10);
+	// Pin 2.10 als Interrupt-Quelle (EINT0) einrichten
+	// -> Setze 20. Bit von PINSEL4 auf 1
+	LPC_PINCON->PINSEL4 |= (1 << 20);
+
+	// Setze EINT0 flankengesteuert (statt level-gesteuert)
+	// -> Setze 1. Bit von EXTMODE auf 1
+	LPC_SC->EXTMODE |= (1 << 0);
+		
+	// EINT0 soll auf steigende Flanke reagieren
+	// -> Setze 1. Bit von EXTPOLAR auf 1
+	LPC_SC->EXTPOLAR |= (1 << 0);
+	
+	// EINT0 soll auf fallende Flanke reagieren
+	// -> Setze 1. Bit von EXTPOLAR auf 0
+	//LPC_SC->EXTPOLAR &= ~(1 << 0);
 
 	// External Interrupt 0 aktivieren
 	NVIC_EnableIRQ(EINT0_IRQn);	
 	
-	while(1){}
-	
-	//	PinGPIO led1 = PinGPIO(LED1_PORT, LED1_PIN, true);
-	//	PinGPIO btn1 = PinGPIO(BTN1_PORT, BTN1_PIN, false);
-	//	
-	//	while(1) {
-	//		if (btn1.steigendeFlanke()) {
-	//			led1.toggle();
-	//		}
-	//	}	
+	while(1) {	}
 }
 //EOF
